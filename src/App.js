6b6aeb9bf7331d1ae42b2cf7a4e249c3dedf67ad
Rishab Khivsara <rishabjain3500@gmail.com>
2020-12-25 12:01:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const animeDB = {
+  noobie: [
+    { name: "Naruto", rating: "4/5" },
+    { name: "Naruto Shippuden", rating: "5/5" }
+  ],
+
+  sliceoflife: [
+    {
+      name: "Mob Pyscho 100",
+      rating: "5/5"
+    },
+    {
+      name: "Your lie in April",
+      rating: "4.5/5"
+    }
+  ],
+  romance: [
+    {
+      name: "Weathering With you",
+      rating: "5/5"
+    },
+    {
+      name: "I want to eat your pancreas",
+      rating: "5/5"
+    }
+  ],
+
+  Psychological: [
+    {
+      name: "Death Note",
+      rating: "5/5"
+    },
+    {
+      name: "The promised Neverland",
+      rating: "4.5/5"
+    }
+  ]
+};
+
+export default function App() {
+  const [selectedGenre, setGenre] = useState("noobie");
+
+  function clickHandler(genre) {
+    setGenre(genre);
+  }
+  return (
+    <div className="App">
+      <h1>BonitoFlakes Anime Recommendation</h1>
+      {/*  */}
+      <div className="anime_button">
+        {Object.keys(animeDB).map((item) => (
+          <button onClick={() => clickHandler(item)} className={item}>
+            {item}
+          </button>
+        ))}
+      </div>
+      {/*  */}
+      <div>
+        <ul>
+          {animeDB[selectedGenre].map((book) => (
+            <li
+              key={book.name}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                border: "1px solid #D1D5DB",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.5rem"
+              }}
+            >
+              <div style={{ fontSize: "larger", textAlign: "left" }}>
+                {book.name}
+              </div>
+              <div style={{ fontSize: "smaller", textAlign: "left" }}>
+                {book.rating}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/*  */}
+    </div>
+  );
+}
